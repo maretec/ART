cd82c7ff88c0828b5502e51c04585bbbda29cde3
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2019-04-30 09:37:58 +0100

    validator for yamlsection

--- art.py
+++ art.py
@@ -1,7 +1,32 @@
 import yaml
+import sys
 
-with open("file.yaml", 'r') as ymlfile:
-    cfg = yaml.safe_load(ymlfile)
 
-for section in cfg:
-  print(cfg[section])
+def openYamlFile(path):
+  with open(path, 'r') as ymlfile:
+    return (yaml.safe_load(ymlfile))
+
+def validateYamlSection(dict, sectionName, mandatoryList):
+      mandatoryParameters = set(mandatoryList).difference(set(dict[sectionName]))
+      if len(mandatoryParameters) != 0:
+        raise ValueError("Missing mandatory parameter on " + sectionName + ": " + str(mandatoryParameters) + ".")
+      if None in list(dict[sectionName].values()):
+        raise ValueError("Blank value on " + sectionName + ".")
+
+def validatorYamlFile(yamlDict):
+  validateYamlSection(yamlDict, 'artconfig', ['mainPath','startDate', 'endDate'])
+  validateYamlSection(yamlDict, 'Mohid', [])
+  for model in yamlDict['models']:
+    validateYamlSection(yamlDict['models'], model, [])
+
+def main():
+  try:
+    cfg = openYamlFile("default.yaml")
+    validatorYamlFile(cfg)
+  except ValueError as e:
+    print(e)
+    sys.exit()
+    
+
+if __name__== "__main__":
+  main()
