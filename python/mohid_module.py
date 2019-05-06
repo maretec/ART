91883aad4dacfa71018c62a8d21dcf86190b6dc7
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2019-05-06 01:33:51 +0100

    adds more functions to module mohid (needs container class)

--- python/mohid_module.py
+++ python/mohid_module.py
@@ -1,10 +1,11 @@
 import yaml_lib
 import datetime
 import logger
+import os.path
 
 logger = logger.ArtLogger("mohid","log.txt")
 
-def validateDate(yaml):
+def validate_date(yaml):
   startDate = datetime.datetime.strptime(yaml['artconfig']['startDate'], '%Y %m %d %H %M %S')
   endDate = datetime.datetime.strptime(yaml['artconfig']['endDate'], '%Y %m %d %H %M %S')
   totalDays = yaml['artconfig']['daysPerRun'] * yaml['artconfig']['numberOfRuns']
@@ -17,6 +18,41 @@ def validateDate(yaml):
     logger.log("all gucci")
   return
 
-def runMohid(yaml):
-  validateDate(yaml)
-validateDate(yaml_lib.openYamlFile("default.yaml"))
+def validate_path(path):
+  return os.path.exists(path)
+
+def running_mode(yaml):
+  if not yaml['artconfig']['classiMode']:
+    if yaml['artconfig']['forecastMode']:
+      today = datetime.datetime.today
+      global_initial_date = today + datetime.timedelta(days = yaml['artconfig']['refDayToStart'])
+      global_final_date = (today + datetime.timedelta(days = yaml['artconfig']['numberOfRuns'])
+        + datetime.timedelta(days = yaml['artconfig']['daysPerRun']))
+    
+      initial_date = global_initial_date
+      final_date = today + datetime.timedelta(days = yaml['artconfig']['daysPerRun'])
+    elif not (yaml['artconfig']['forecastMode']):
+      startDate = datetime.datetime.strptime(yaml['artconfig']['startDate'], '%Y %m %d %H %M %S')
+      endDate = datetime.datetime.strptime(yaml['artconfig']['endDate'], '%Y %m %d %H %M %S')
+    
+      difference = endDate - startDate
+      numberOfRuns = difference.days
+    else:
+      raise ValueError("artconfig: forecastMode value needs to be either a number or true/false")    
+  elif yaml['artconfig']['classicMode']:
+    numberOfRuns = 1
+
+    start_date = datetime.datetime.strptime(yaml['artconfig']['startDate'], '%Y %m %d %H %M %S')
+    end_date = datetime.datetime.strptime(yaml['artconfig']['endDate'], '%Y %m %d %H %M %S')
+    
+    initial_date = start_date
+    final_date = end_date
+  else:
+    raise ValueError("artconfig: classicMode value needs to be either a number or true/false")
+
+def run_mohid(yaml):
+  validate_date(yaml)
+  validate_path(yaml['artconfig']['mainPath'])
+
+yaml = yaml_lib.openYamlFile('default.yaml')
+running_mode(yaml)
