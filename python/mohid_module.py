6433bd8c959dcb829e65a5c5437f20d84364d592
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2019-05-07 11:16:36 +0100

    adds run Mohid commands

--- python/mohid_module.py
+++ python/mohid_module.py
@@ -1,58 +1,85 @@
 import yaml_lib
 import datetime
 import logger
-import os.path
+import os.path, subprocess
 
 logger = logger.ArtLogger("mohid","log.txt")
 
+
 def validate_date(yaml):
-  startDate = datetime.datetime.strptime(yaml['artconfig']['startDate'], '%Y %m %d %H %M %S')
-  endDate = datetime.datetime.strptime(yaml['artconfig']['endDate'], '%Y %m %d %H %M %S')
-  totalDays = yaml['artconfig']['daysPerRun'] * yaml['artconfig']['numberOfRuns']
-  
-  if startDate + datetime.timedelta(days= totalDays) > endDate:
-    raise ValueError("artconfig: The number of daysPerRun (" + str(yaml['artconfig']['daysPerRun']) + 
-    ") in conjunction with the numberOfRuns (" + str(yaml['artconfig']['numberOfRuns']) + ") plus the startDate of this run (" + str(startDate) + 
-    ") would lead to a final date of simulation beyond the user-specified endDate (" + str(endDate) + ").")
-  else:
-    logger.log("all gucci")
-  return
+    startDate = datetime.datetime.strptime(yaml['artconfig']['startDate'], '%Y %m %d %H %M %S')
+    endDate = datetime.datetime.strptime(yaml['artconfig']['endDate'], '%Y %m %d %H %M %S')
+    totalDays = yaml['artconfig']['daysPerRun'] * yaml['artconfig']['numberOfRuns']
+
+    if startDate + datetime.timedelta(days= totalDays) > endDate:
+        raise ValueError("artconfig: The number of daysPerRun (" + str(yaml['artconfig']['daysPerRun']) +
+                         ") in conjunction with the numberOfRuns (" + str(yaml['artconfig']['numberOfRuns']) + ") plus the startDate of this run (" + str(startDate) +
+                         ") would lead to a final date of simulation beyond the user-specified endDate (" + str(endDate) + ").")
+    else:
+        logger.log("all gucci")
+    return
+
 
 def validate_path(path):
-  return os.path.exists(path)
+    return os.path.exists(path)
+
 
 def running_mode(yaml):
-  if not yaml['artconfig']['classiMode']:
-    if yaml['artconfig']['forecastMode']:
-      today = datetime.datetime.today
-      global_initial_date = today + datetime.timedelta(days = yaml['artconfig']['refDayToStart'])
-      global_final_date = (today + datetime.timedelta(days = yaml['artconfig']['numberOfRuns'])
-        + datetime.timedelta(days = yaml['artconfig']['daysPerRun']))
-    
-      initial_date = global_initial_date
-      final_date = today + datetime.timedelta(days = yaml['artconfig']['daysPerRun'])
-    elif not (yaml['artconfig']['forecastMode']):
-      startDate = datetime.datetime.strptime(yaml['artconfig']['startDate'], '%Y %m %d %H %M %S')
-      endDate = datetime.datetime.strptime(yaml['artconfig']['endDate'], '%Y %m %d %H %M %S')
-    
-      difference = endDate - startDate
-      numberOfRuns = difference.days
+    if not yaml['artconfig']['classiMode']:
+        if yaml['artconfig']['forecastMode']:
+            today = datetime.datetime.today
+            global_initial_date = today + datetime.timedelta(days = yaml['artconfig']['refDayToStart'])
+            global_final_date = (today + datetime.timedelta(days = yaml['artconfig']['numberOfRuns'])
+                                 + datetime.timedelta(days = yaml['artconfig']['daysPerRun']))
+
+            initial_date = global_initial_date
+            final_date = today + datetime.timedelta(days = yaml['artconfig']['daysPerRun'])
+        elif not (yaml['artconfig']['forecastMode']):
+            startDate = datetime.datetime.strptime(yaml['artconfig']['startDate'], '%Y %m %d %H %M %S')
+            endDate = datetime.datetime.strptime(yaml['artconfig']['endDate'], '%Y %m %d %H %M %S')
+
+            difference = endDate - startDate
+            numberOfRuns = difference.days
+        else:
+            raise ValueError("artconfig: forecastMode value needs to be either a number or true/false")
+    elif yaml['artconfig']['classicMode']:
+        numberOfRuns = 1
+
+        start_date = datetime.datetime.strptime(yaml['artconfig']['startDate'], '%Y %m %d %H %M %S')
+        end_date = datetime.datetime.strptime(yaml['artconfig']['endDate'], '%Y %m %d %H %M %S')
+
+        initial_date = start_date
+        final_date = end_date
     else:
-      raise ValueError("artconfig: forecastMode value needs to be either a number or true/false")    
-  elif yaml['artconfig']['classicMode']:
-    numberOfRuns = 1
+        raise ValueError("artconfig: classicMode value needs to be either a number or true/false")
 
-    start_date = datetime.datetime.strptime(yaml['artconfig']['startDate'], '%Y %m %d %H %M %S')
-    end_date = datetime.datetime.strptime(yaml['artconfig']['endDate'], '%Y %m %d %H %M %S')
-    
-    initial_date = start_date
-    final_date = end_date
-  else:
-    raise ValueError("artconfig: classicMode value needs to be either a number or true/false")
 
-def run_mohid(yaml):
-  validate_date(yaml)
-  validate_path(yaml['artconfig']['mainPath'])
+def mpi_params(yaml_file):
+    mohid = yaml_file['mohid']
+    if 'mpi' in mohid.keys():
+        if mohid['mpi']['enable']:
+            exe_path = mohid['mpi']['exePath']
+            keepDecomposedFiles = mohid['mpi']['keepDecomposedFiles']
+            ddcParserNumProcessors = mohid['mpi']['ddcComposerNumProcessors']
+            joinerVersion = mohid['mpi']['joinerVersion']
 
-yaml = yaml_lib.openYamlFile('default.yaml')
-running_mode(yaml)
+
+def run_mohid(yaml, model):
+    validate_date(yaml)
+    validate_path(yaml['artconfig']['mainPath'])
+    if 'mpi' in yaml['mohid'].keys() and yaml['mohid']['mpi']['enable']:
+        mpi = yaml['mohid']['mpi']
+        flags = " -n " + str(yaml['mohid']['models'][model]['mpiProcessors']) + " -f /opt/hosts " +\
+            yaml['mohid']['exePath']
+        logger.log("Starting MOHID MPI run of model: " + yaml['mohid']['models'][model]['name'])
+        subprocess.run([mpi['exePath'], flags])
+        logger.log("MOHID MPI run finished")
+    else:
+        logger.log("Starting MOHID run of model " + yaml['mohid']['models'][model]['name'])
+        subprocess.run(yaml['mohid']['exePath'])
+        logger.log("MOHID run finished")
+
+
+
+yaml = yaml_lib.open_yaml_file('default.yaml')
+run_mohid(yaml, 'model1')
--- python/yaml_lib.py
+++ python/yaml_lib.py
@@ -1,25 +1,28 @@
 import yaml
-import sys
 
-def openYamlFile(path):
-  with open(path, 'r') as ymlfile:
-    return (yaml.safe_load(ymlfile))
 
-def validateYamlSection(dict, sectionName, mandatoryList):
-  mandatoryParameters = set(mandatoryList).difference(set(dict[sectionName]))
-  if len(mandatoryParameters) != 0:
-    raise ValueError("Missing mandatory parameter on " + sectionName + ": " + str(mandatoryParameters) + ".")
-  if None in list(dict[sectionName].values()):
-    raise ValueError("Blank value on " + sectionName + ".")
+def open_yaml_file(path):
+    with open(path, 'r') as yml_file:
+        return yaml.safe_load(yml_file)
 
-def validateYamlFile(yamlDict):
-  validateYamlSection(yamlDict, 'artconfig', ['mainPath','startDate', 'endDate', 'runPreProcessing','runMohid', 'runPostProcessing'])
-  if yamlDict['artconfig']['runMohid']:
-    validateYamlSection(yamlDict, 'Mohid', [])
-    if 'mpi' in yamlDict['Mohid']:
-          validateYamlSection(yamlDict['Mohid'], 'mpi',[])
-    for model in yamlDict['Mohid']['Models']:
-      validateYamlSection(yamlDict['Mohid']['Models'], model, [])
 
-def readAttribute(cfg, attribute):
-  return cfg[attribute]
+def validate_yaml_section(yaml_file, section_name, mandatory_list):
+    mandatory_parameters = set(mandatory_list).difference(set(yaml_file[section_name]))
+    if len(mandatory_parameters) != 0:
+        raise ValueError("Missing mandatory parameter on " + section_name + ": " + str(mandatory_parameters) + ".")
+    if None in list(yaml_file[section_name].values()):
+        raise ValueError("Blank value on " + section_name + ".")
+
+
+def validate_yaml_file(yaml_dict):
+    validate_yaml_section(yaml_dict, 'artconfig', ['mainPath','startDate', 'endDate', 'runPreProcessing','runMohid', 'runPostProcessing'])
+    if yaml_dict['artconfig']['runMohid']:
+        validate_yaml_section(yaml_dict, 'Mohid', [])
+        if 'mpi' in yaml_dict['Mohid']:
+            validate_yaml_section(yaml_dict['Mohid'], 'mpi',[])
+        for model in yaml_dict['Mohid']['Models']:
+            validate_yaml_section(yaml_dict['Mohid']['Models'], model, [])
+
+
+def read_attribute(cfg, attribute):
+    return cfg[attribute]
