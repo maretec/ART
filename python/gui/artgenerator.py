e95bb574bd2dc135f5a3d2c2084dc2aaa53c000e
Filipe Martins <fmfm.97@hotmail.com>
2019-05-27 17:29:55 +0100

    restructure gui to an oop approach

---
+++ python/gui/artgenerator.py
@@ -0,0 +1,105 @@
+import tkinter as tk
+import tkcalendar
+from tkinter import filedialog
+from tkinter import ttk
+
+
+class ArtGenApp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title("ART Config Generator for MOHID")
+        style = ttk.Style()
+        style.theme_use('clam')
+        container = tk.Frame(self)
+        container.grid()
+        self.frames = {}
+
+        for F in (StartPage, NewCfg):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        start_lbl = ttk.Label(self, text="Welcome to Automatic Running Tool's MOHID Configuration Generator")
+        start_lbl.pack(pady=10, padx=10)
+
+        newcfg_btn = ttk.Button(self, text="Create new configuration", command=lambda: controller.show_frame(NewCfg))
+        newcfg_btn.pack()
+
+
+class NewCfg(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        self.main_path = tk.StringVar()
+
+        mainpath_dir_lbl = ttk.Label(self, text="Main Path")
+        mainpath_dir_lbl.grid(row=0, column=0)
+        mainpath_dir_entry = ttk.Entry(self, textvariable=self.main_path)
+        mainpath_dir_entry.grid(row=0, column=1)
+        mainpath_dir_btn = ttk.Button(self, text="Browse", command=lambda: browse_button(self.main_path))
+        mainpath_dir_btn.grid(row=0, column=2)
+
+        mode_lbl = ttk.Label(self, text="Mode")
+        mode_lbl.grid(row=1, column=0)
+        self.mode_list = {"Classic", "Forecast", "Hindcast"}
+        mode_choice = tk.StringVar()
+        mode_choice.set("Forecast")
+        mode_popup_menu = ttk.OptionMenu(self, mode_choice, next(iter(self.mode_list)), *self.mode_list)
+        mode_popup_menu.grid(row=1, column=1)
+
+        days_per_run_lbl = ttk.Label(self, text="Days per Run")
+        days_per_run_lbl.grid(row=2, column=0)
+        days_per_run_entry = ttk.Entry(self)
+        days_per_run_entry.grid(row=2, column=1)
+
+        ref_day_to_start_lbl = ttk.Label(self, text="Reference Day to Start")
+        ref_day_to_start_lbl.grid(row=3, column=0)
+        ref_day_to_start_entry = ttk.Entry(self)
+        ref_day_to_start_entry.grid(row=3, column=1)
+
+        num_of_runs_lbl = ttk.Label(self, text="Number of Runs")
+        num_of_runs_lbl.grid(row=4, column=0)
+        num_of_runs_entry = ttk.Entry(self)
+        num_of_runs_entry.grid(row=4, column=1)
+
+        self.run_pre_processing_bool = tk.BooleanVar()
+        # run_pre_processing_lbl = ttk.Label(self, text="Pre-processing")
+        run_pre_processing_cbox = ttk.Checkbutton(self, text="Pre-processing", variable=self.run_pre_processing_bool,
+                                                  onvalue=True, offvalue=False)
+        run_pre_processing_cbox.grid(row=5, column=0)
+        self.run_post_processing_bool = tk.BooleanVar()
+        run_post_processing_cbox = ttk.Checkbutton(self, text="Post-processing", variable=self.run_post_processing_bool,
+                                                   onvalue=True, offvalue=False)
+        run_post_processing_cbox.grid(row=6, column=0)
+
+        start_date_lbl = ttk.Label(self, text="Start Date")
+        start_date_lbl.grid(row=7, column=0)
+        start_date_entry = tkcalendar.DateEntry(self, year=2018)
+        start_date_entry.grid(row=7, column=1)
+
+        end_date_lbl = ttk.Label(self, text="End Date")
+        end_date_lbl.grid(row=8, column=0)
+        end_date_entry = tkcalendar.DateEntry(self, year=2018)
+        end_date_entry.grid(row=8, column=1)
+
+
+def browse_button(path):
+    directory = filedialog.askdirectory()
+    path.set(directory)
+    print(directory)
+
+
+app = ArtGenApp()
+app.mainloop()
--- python/gui/helloworld.py
+++ python/gui/helloworld.py
@@ -1,35 +1,52 @@
-import tkinter
+import tkinter as tk
+import tkcalendar
 from tkinter import filedialog
 
 main_path = None
 
 
-
-
 def main():
     global main_path
-    window = tkinter.Tk()
+    window = tk.Tk()
     window.title("ART Config Generator for MOHID")
-    window.wm_attributes('-zoomed', 1)
-    top_frame = tkinter.Frame(window).grid()
-    main_path = tkinter.StringVar()
+    # window.wm_attributes('-zoomed', 1)
+    top_frame = tk.Frame(window).grid()
+    main_path = tk.StringVar()
 
-    mainpath_dir_lbl = tkinter.Label(top_frame, text="Main Path").grid(row=0, column=0)
-    mainpath_dir_entry = tkinter.Entry(top_frame, textvariable=main_path).grid(row=0, column=1)
-    mainpath_dir_btn = tkinter.Button(top_frame, text="Browse", command=browse_button).grid(row=0, column=2)
+    mainpath_dir_lbl = tk.Label(top_frame, text="Main Path").grid(row=0, column=0)
+    mainpath_dir_entry = tk.Entry(top_frame, textvariable=main_path).grid(row=0, column=1)
+    mainpath_dir_btn = tk.Button(top_frame, text="Browse", command=browse_button).grid(row=0, column=2)
 
-    mode_lbl = tkinter.Label(top_frame, text="Mode").grid(row=1, column=0)
-    mode_menu = tkinter.Menu(top_frame)
+    mode_lbl = tk.Label(top_frame, text="Mode").grid(row=1, column=0)
+    mode_menu = tk.Menu(top_frame)
     mode_list = {"Classic", "Forecast", "Hindcast"}
-    mode_choice = tkinter.StringVar(top_frame)
+    mode_choice = tk.StringVar(top_frame)
     mode_choice.set("Forecast")
-    mode_popup_menu = tkinter.OptionMenu(top_frame, mode_choice, *mode_list).grid(row=1, column=1)
+    mode_popup_menu = tk.OptionMenu(top_frame, mode_choice, *mode_list).grid(row=1, column=1)
     mode_choice.trace('w', change_dropdown(mode_choice))
 
+    days_per_run_lbl = tk.Label(top_frame, text="Days per Run").grid(row=2, column=0)
+    days_per_run_entry = tk.Entry(top_frame).grid(row=2, column=1)
 
+    ref_day_to_start_lbl = tk.Label(top_frame, text="Reference Day to Start").grid(row=3, column=0)
+    ref_day_to_start_entry = tk.Entry(top_frame).grid(row=3, column=1)
 
+    num_of_runs_lbl = tk.Label(top_frame, text="Number of Runs").grid(row=4, column=0)
+    num_of_runs_entry = tk.Entry(top_frame).grid(row=4, column=1)
 
+    run_pre_processing_bool = tk.BooleanVar()
+    # run_pre_processing_lbl = tk.Label(top_frame, text="Pre-processing")
+    run_pre_processing_cbox = tk.Checkbutton(top_frame, text="Pre-processing", variable=run_pre_processing_bool,
+                                                  onvalue=True, offvalue=False).grid(row=5, column=0)
+    run_post_processing_bool = tk.BooleanVar()
+    run_post_processing_cbox = tk.Checkbutton(top_frame, text="Post-processing", variable=run_post_processing_bool,
+                                                   onvalue=True, offvalue=False).grid(row=6, column=0)
 
+    start_date_lbl = tk.Label(top_frame, text="Start Date").grid(row=7, column=0)
+    start_date_entry = tkcalendar.DateEntry(top_frame, year=2018).grid(row=7, column=1)
+
+    end_date_lbl = tk.Label(top_frame, text="End Date").grid(row=8, column=0)
+    end_date_entry = tkcalendar.DateEntry(top_frame, year=2018).grid(row=8, column=1)
 
     window.mainloop()
 
@@ -39,6 +56,7 @@ def browse_button():
     directory = filedialog.askdirectory()
     main_path.set(directory)
     print(directory)
+
 
 def change_dropdown(var):
     print(var.get())
