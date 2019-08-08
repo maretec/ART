33966c851aa50db167abe29d1946ae0dbf636d8d
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2019-08-08 16:33:17 +0100

    fix on dict call

--- python/run_modules/pre_processing.py
+++ python/run_modules/pre_processing.py
@@ -15,7 +15,7 @@ def execute(yaml):
     block_keys = yaml['preProcessing'][block].keys()
     block_dict = yaml['preProcessing'][block]
     if block_dict['run']:
-      if 'datDataChange' in  block_keys and block_dict['datDataChange']:
+      if 'datDateChange' in  block_keys and block_dict['datDataChange']:
         print("debug")
         dat_date_change(block_dict['configFilePath'])
       if 'flags' in block_keys:
