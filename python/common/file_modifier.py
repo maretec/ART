88812b26ffbaa0ed1994f4367fbd90fe77ed9788
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2019-08-20 11:13:23 +0100

    Revert "structure changes - new branch"
    This reverts commit 85ac63a086b781058e9f43f1c72d701c95a2e568.

--- python/common/file_modifier.py
+++ python/common/file_modifier.py
@@ -2,8 +2,7 @@ import fileinput
 import sys
 
 
-def line_creator(filepath, parameter, value):
-    file = open(filepath, "w+")
+def line_creator(file, parameter, value):
     file.write('{0:30}{1}'.format(parameter, ": " + value + "\n"))
     file.flush()
 
@@ -17,9 +16,7 @@ def modify_line(filepath, parameter, new_value):
     for line in fileinput.input(filepath, inplace=True):
         if parameter in line:
             line = parameter + " : " + new_value + "\n"
-            return
         sys.stdout.write(line)
-    line_creator(filepath, parameter, new_value)
 
 
 def nomfinch_creator(workPath, parameters, values):
