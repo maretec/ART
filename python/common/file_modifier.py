441ca2cf6cdd426753e7ea201dd24da0450cdc7f
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2019-08-20 15:58:20 +0100

    fix close on file modifier

--- python/common/file_modifier.py
+++ python/common/file_modifier.py
@@ -6,6 +6,7 @@ def line_creator(filepath, parameter, value):
     file = open(filepath, "w+")
     file.write('{0:30}{1}'.format(parameter, ": " + value + "\n"))
     file.flush()
+    file.close()
 
 def date_to_mohid_date(date):
     mohid_date = str(date.year) + " " + str(date.month) + " " + str(date.day) + " " + str(0) + " " + \
