80a53dcaeecaf45ec264907a8320a9e616555481
tomasalagoa <tomas.alagoa@gmail.com>
2019-08-29 16:46:52 +0100

    Update

--- python/common/validator.py
+++ python/common/validator.py
@@ -1,16 +1,18 @@
 import os
 
 valid = True
-artconfig_modules = ['mainPath', 'forecastMode', 'daysPerRun', 'refDaysToStart', 'numberOfRuns', 'runPreProcessing', 'startDate', 'endDate', 'sendEmail']
+general_keys = ['artconfig', 'mohid', 'model', 'discharges', 'obc', 'meteo', 'model.dat', 'preprocessing', 'postprocessing']
+artconfig_modules = ['mainPath', 'operationalMode', 'runPreProcessing', 'daysPerRun', 'refDaysToStart', 'numberOfRuns', 'module', 'runSimulation', 'startDate', 'endDate', 'ouputFile', 'outputFilePath', 'sendEmail', 'email']
+mohid_modules = ['maxTime', 'exePath', 'outputToFile', 'outputFilePath', 'mpi']
 mpi_modules = ['enable', 'numDomains', 'exePath', 'keepDecomposedFile', 'ddcComposerNumProcessors', 'joinerVersion']
-mohid_modules = ['maxTime', 'outputFile', 'outputPath', 'exePath']
 model_modules = ['name', 'path', 'mpiProcessors', 'obc', 'meteo', 'gridFile', 'runId', 'DT', 'backupPath', 'storagePath', 'hasWaterProperties', 'hasSurfaceHDF', 'hasGOTM', 'hasOutputWindow', 'hasSolutionFromFile']
 obc_modules = ['enable', 'modelName', 'simulatedDays']
 meteo_modules = ['enable', 'modelName', 'simulatedDays', 'fileNameModel', 'workPath']
 
-def artconfig(yaml):
-    if 'artconfig' not in yaml.keys():
-        valid = False
+def validate(yaml):
+    for i in general_keys:
+        if i not in yaml.keys():
+            valid = False
 
     for i in artconfig_modules:
         if i not in yaml['artconfig'].keys():
