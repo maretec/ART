b60ba497effe702c1c1d8be84b1537b456f4345b
tomasalagoa <tomas.alagoa@gmail.com>
2019-08-29 17:49:42 +0100

    Update

--- python/common/validator.py
+++ python/common/validator.py
@@ -1,48 +1,89 @@
 import os
 
-valid = True
 general_keys = ['artconfig', 'mohid', 'model', 'discharges', 'obc', 'meteo', 'model.dat', 'preprocessing', 'postprocessing']
 artconfig_modules = ['mainPath', 'operationalMode', 'runPreProcessing', 'daysPerRun', 'refDaysToStart', 'numberOfRuns', 'module', 'runSimulation', 'startDate', 'endDate', 'ouputFile', 'outputFilePath', 'sendEmail', 'email']
 mohid_modules = ['maxTime', 'exePath', 'outputToFile', 'outputFilePath', 'mpi']
-mpi_modules = ['enable', 'numDomains', 'exePath', 'keepDecomposedFile', 'ddcComposerNumProcessors', 'joinerVersion']
-model_modules = ['name', 'path', 'mpiProcessors', 'obc', 'meteo', 'gridFile', 'runId', 'DT', 'backupPath', 'storagePath', 'hasWaterProperties', 'hasSurfaceHDF', 'hasGOTM', 'hasOutputWindow', 'hasSolutionFromFile']
-obc_modules = ['enable', 'modelName', 'simulatedDays']
-meteo_modules = ['enable', 'modelName', 'simulatedDays', 'fileNameModel', 'workPath']
+mpi_modules = ['enable', 'exePath', 'totalProcessors']
+model_modules = ['name', 'path', 'gridFile', 'dt', 'storagePath']
+discharges_modules = ['enable', 'path', 'dateFormat']
+obc_modules = ['enable', 'suffix', 'hasSolutionFromFile', 'prefix', 'dateFormat', 'filetype', 'simulatedDays', 'subFolders', 'workPath']
+meteo_modules = ['enable']
+models_modules = ['uniqueId', 'name', 'simulatedDays', 'fileNameFromModel', 'workPath', 'dateFormat', 'fileType']
+modeldat_modules = ['MAXDT', 'GMTREFERENCE', 'DT_PREDICTION_INTERVAL']
+preprocessing_modules = ['name of block', 'run', 'datDateChange', 'configFilePath', 'exePath', 'flags', 'outputToFile', 'outputFilePath']
+postprocessing_modules = ['name of block', 'run', 'datDateChange', 'configFilePath', 'exePath', 'flags', 'outputToFile', 'outputFilePath']
 
-def validate(yaml):
+def validatePath(path):
+    return os.path.exists(path) and path[-1] == '/'
+
+def general_validation(yaml):
+    valid = True
     for i in general_keys:
         if i not in yaml.keys():
             valid = False
 
+    return valid
+
+
+def artconfig_validation(yaml):
+    valid = True
     for i in artconfig_modules:
         if i not in yaml['artconfig'].keys():
             valid = False
 
-    if 'mohid' not in yaml.keys():
-        valid = False
 
-    if 'mpi' not in yaml['mohid']:
-        valid = False
+
+def mohid_validation(yaml):
+    valid = True
+    for i in mohid_modules:
+        if i not in yaml['mohid'].keys():
+            valid = False
 
     for i in mpi_modules:
         if i not in yaml['mohid']['mpi'].keys():
             valid = False
-    
-    for i in mohid_modules:
-        if i not in yaml['mohid'].keys():
+
+def discharge_validation(yaml):    
+    valid = True
+    for i in discharges_modules:
+        if i not in yaml['discharge'].keys():
             valid = False
-    
-    if 'model' not in yaml.keys():
-        valid = False
+
+
+def obc_validation(yaml):
+    valid = True
+    for i in obc_modules:
+        if i not in yaml['obc'].keys():
+            valid = False
+
+
+def meteo_validation(yaml):
+    valid = True
+    for i in meteo_modules:
+        if i not in yaml['meteo'].keys():
+            valid = False
     
     for i in model_modules:
-        if i not in yaml['model'].keys():
+        if i not in yaml['meteo']['models'].keys():
             valid = False
     
-    for i in obc_modules:
-        if i not in yaml['model']['obc'].keys():
+
+def modeldat_validation(yaml):
+    valid = True
+    for i in modeldat_modules:
+        if i not in yaml['model.dat'].keys():
             valid = False
-    
-    for i in meteo_modules:
-        if i not in yaml['model']['meteo'].keys():
+
+
+def preprocessing_validation(yaml):
+    valid = True
+    for i in preprocessing_modules:
+        if i not in yaml['preprocessing'].keys():
+            valid = False
+
+
+def postprocessing_validation(yaml):
+    valid = True
+    for i in postprocessing_modules:
+        if i not in yaml['postprocessing'].keys():
             valid = False
