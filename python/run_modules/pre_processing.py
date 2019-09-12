b109f505a54856dfd17f62f2774ce89dc507d318
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2019-09-12 13:01:59 +0100

    same fix on pre_processing

--- python/run_modules/pre_processing.py
+++ python/run_modules/pre_processing.py
@@ -22,16 +22,16 @@ def execute(yaml):
         run_array = [block_dict['exePath']] + flags_array
         if 'outputToFile' in block_keys and block_dict['outputToFile']:
           with open(block_dict['outputFilePath'], 'w') as log:
-            subprocess.run(run_array, stdout=log, cwd=os.path.dirname(block_dict['exePath']))
+            subprocess.run(run_array, stdout=log, block_dict['workingDirectory'])
             log.close()
       else:
         if 'outputToFile' in block_keys and block_dict['outputToFile']:
          with open(block_dict['outputFilePath'], 'w') as log:
             static.logger.info("Executing Pre Processing module: " +  block_dict['exePath'])
-            subprocess.run(block_dict['exePath'], stdout=log, cwd=os.path.dirname(block_dict['exePath']))
+            subprocess.run(block_dict['exePath'], stdout=log, block_dict['workingDirectory'])
             log.close()
         else:
           static.logger.info("Executing Pre Processing module: " +  block_dict['exePath'])
-          subprocess.run(block_dict['exePath'], cwd=os.path.dirname(block_dict['exePath']))
+          subprocess.run(block_dict['exePath'], block_dict['workingDirectory'])
 
   return  
