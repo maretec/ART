168e85530f9d208478758addf48809b9f62a169e
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2019-09-12 12:35:50 +0100

    fixes post_processing

--- python/run_modules/post_processing.py
+++ python/run_modules/post_processing.py
@@ -1,38 +1,37 @@
 import subprocess
 import common.file_modifier as file_modifier
 import common.config as cfg
+import os
 import common.constants as static
 
 
+
 def dat_date_change(filePath):
-  file_modifier.modify_line(filePath, "START", cfg.current_initial_date)
-  file_modifier.modify_line(filePath, "END", cfg.current_final_date)
+  file_modifier.modify_line(filePath, "START", file_modifier.date_to_mohid_date(cfg.current_initial_date))
+  file_modifier.modify_line(filePath, "END", file_modifier.date_to_mohid_date(cfg.current_initial_date))
 
 def execute(yaml):
-  
   for block in yaml['postProcessing']:
-    block_keys = yaml['postProcessing']['block'].keys()
-    block_dict = yaml['postProcessing']['block']
+    block_keys = yaml['postProcessing'][block].keys()
+    block_dict = yaml['postProcessing'][block]
     if block_dict['run']:
-      if 'datDataChange' in  block_keys and block_dict['datDataChange']:
+      if 'datDateChange' in  block_keys and block_dict['datDateChange']:
         dat_date_change(block_dict['configFilePath'])
       if 'flags' in block_keys:
         flags_array = block_dict['flags'].split(" ") 
         run_array = [block_dict['exePath']] + flags_array
         if 'outputToFile' in block_keys and block_dict['outputToFile']:
           with open(block_dict['outputFilePath'], 'w') as log:
-            static.logger.info("Executing Post Processing module: " +  block_dict['exePath'])
-            subprocess.run(run_array, stdout=log)
+            subprocess.run(run_array, stdout=log, cwd=os.path.dirname(block_dict['exePath']))
             log.close()
       else:
         if 'outputToFile' in block_keys and block_dict['outputToFile']:
-     
          with open(block_dict['outputFilePath'], 'w') as log:
             static.logger.info("Executing Post Processing module: " +  block_dict['exePath'])
-            subprocess.run(run_array, stdout=log)
+            subprocess.run(block_dict['exePath'], stdout=log, cwd=os.path.dirname(block_dict['exePath']))
             log.close()
         else:
           static.logger.info("Executing Post Processing module: " +  block_dict['exePath'])
-          subprocess.run(block_dict['exePath'])
+          subprocess.run(block_dict['exePath'], cwd=os.path.dirname(block_dict['exePath']))
 
   return  
