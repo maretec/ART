e6fc2b2a7a8c850bd717452847632e44a9046b4f
tomasalagoa <tomas.alagoa@gmail.com>
2019-10-10 12:51:36 +0100

    Update

--- python/common/create_yaml.py
+++ python/common/create_yaml.py
@@ -14,36 +14,36 @@ postprocessing_modules = ['#name of block']
 name_of_block_modules_post=['RUN', 'DAT_DATE_CHANGE', 'WORKING_DIRECTORY', 'CONFIG_FILEPATH', 'EXE_PATH', 'FLAGS', 'OUTPUT_TO_FILE', 'OUTPUT_FILEPATH']
 
 def create_artconfig_block(filename):
-    filename.write("artconfig:\n")
+    filename.write("ARTCONFIG:\n")
     for i in artconfig_modules:
         filename.write("  " + i + ":\n")
 
 def create_mohid_block(filename):
-    filename.write("mohid:\n")
+    filename.write("MOHID:\n")
     for i in mohid_modules:
         filename.write("  " + i + ":\n")
     for i in mpi_modules:
         filename.write("    " + i + ":\n")
 
 def create_model_block(filename):
-    filename.write("model:\n")
+    filename.write("MODEL:\n")
     for i in model_modules:
         filename.write("  " + i + ":\n")
 
 def create_discharges_block(filename):
-    filename.write("  discharges:\n")
+    filename.write("  DISCHARGES:\n")
     for i in discharges_modules:
         filename.write("    " + i + ":\n")
 
 def create_obc_block(filename):
-    filename.write("  obc:\n")
+    filename.write("  OBC:\n")
     for i in obc_modules:
         if i == "files":
             filename.write("    " + i + ": [<write list of files you want from the OBC workpath (e.g. ['Hydrodynamic', 'WaterProperties']). Different files require a new list>]\n")
         filename.write("    " + i + ":\n")
 
 def create_meteo_block(filename, n):
-    filename.write("  meteo:\n")
+    filename.write("  METEO:\n")
     for i in meteo_modules:
         filename.write("    " + i + ":\n")
     for _ in range(n):
@@ -52,38 +52,38 @@ def create_meteo_block(filename, n):
             filename.write("        " + i + ":\n")
 
 def create_model_dat_block(filename):
-    filename.write("  model.dat:\n")
+    filename.write("  MODEL.DAT:\n")
     for i in modeldat_modules:
         filename.write("    " + i + ":\n")
 
 def create_preprocessing_block(filename, n):
-    filename.write("preprocessing:\n")
+    filename.write("PREPROCESSING:\n")
     for i in range(n):
         filename.write("  " + preprocessing_modules[0] + ":\n")
         for i in name_of_block_modules_pre:
             filename.write("    " + i + ":\n")
 
 def create_postprocessing_block(filename, n):
-    filename.write("postprocessing:\n")
+    filename.write("POSTPROCESSING:\n")
     for i in range(n):
         filename.write("  " + postprocessing_modules[0] + ":\n")
         for i in name_of_block_modules_post:
             filename.write("    " + i + ":\n")
 
 def create_yaml():
-    filename = input("filename: ")
+    filename = input("FILENAME: ")
     f1 = filename + ".yaml"
     f = open(f1, "w+")
 
-    number_model_blocks = int(input("number of model blocks: "))
+    number_model_blocks = int(input("NUMBER OF MODEL BLOCKS: "))
     number_model_meteo_blocks = []
     
     for i in range(number_model_blocks):
-        t = int(input("number of meteo blocks for model nr." + str(i+1) + " : "))
+        t = int(input("NUMBER OF METEO BLOCKS FOR MODEL BLOCK NR." + str(i+1) + " : "))
         number_model_meteo_blocks.append(t)
 
-    number_preprocessing_blocks = int(input("number of preprocessing blocks: "))
-    number_postprocessing_blocks = int(input("number of postprocessing blocks: "))
+    number_preprocessing_blocks = int(input("NUMBER OF PREPROCESSING BLOCKS: "))
+    number_postprocessing_blocks = int(input("NUMBER OF POSTPROCESSING BLOCKS: "))
 
     create_artconfig_block(f)
     create_mohid_block(f)
