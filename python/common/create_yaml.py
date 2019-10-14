0ce508b8a4730261222ebef6a2a7e501388227e6
tomasalagoa <tomas.alagoa@gmail.com>
2019-10-14 12:06:14 +0100

    Update

--- python/common/create_yaml.py
+++ python/common/create_yaml.py
@@ -1,13 +1,15 @@
-artconfig_modules = ['MAIN_PATH', 'OPERATIONAL_MODE', 'RUN_PRE_PROCESSING', 'DAYS_PER_RUN', 'REF_DAYS_TO_START', 'NUMBER_OF_RUNS', 'MODULE', 'RUN_SIMULATION', 'START_DATE', 'END_DATE', 'OUTPUT_TO_FILE', 'OUTPUT_FILEPATH', 'SEND_EMAIL', 'EMAIL']
+artconfig_modules = ['MAIN_PATH', 'OPERATIONAL_MODE', 'RUN_PRE_PROCESSING', 'DAYS_PER_RUN', 'REF_DAYS_TO_START', 'NUMBER_OF_RUNS', 'MODULE', 'RUN_SIMULATION_MODULE', 'RUN_SIMULATION_POST', 'START_DATE', 'END_DATE', 'OUTPUT_TO_FILE', 'OUTPUT_FILEPATH', 'SEND_EMAIL', 'EMAIL']
 mohid_modules = ['MAX_TIME', 'EXE_PATH', 'OUTPUT_TO_FILE', 'OUTPUT_FiLE_PATH', 'MPI']
 mpi_modules = ['ENABLE', 'EXE_PATH', 'TOTAL_PROCESSORS']
 model_modules = ['NAME', 'PATH', 'GRID_FILE', 'DT', 'STORAGE_PATH', 'RESULTS_LIST', 'HAS_SOLUTION_FROM_FILE']
-discharges_modules = ['ENABLE', 'PATH', 'DATE_FORMAT']
-obc_modules = ['ENABLE', 'FILE_TYPE', 'SIMULATED_DAYS', 'SUB_FOLDERS', 'DATE_IN_FILENAME', 'DATE_FORMAT', 'FILES', 'WORK_PATH']
+discharges_modules = ['#discharge block name']
+discharge_block_modules = ['ENABLE', 'PATH', 'DATE_FORMAT']
+obc_modules = ['#obc block name']
+obc_block_modules = ['ENABLE', 'FILE_TYPE', 'SIMULATED_DAYS', 'SUB_FOLDERS', 'DATE_IN_FILENAME', 'DATE_FORMAT', 'FILES', 'WORKPATH']
 meteo_modules = ['ENABLE', 'MODELS']
 models_modules = ['#put meteo model Id here']
-uniqueId_modules=['NAME', 'SIMULATED_DAYS', 'FILENAME_FROM_MODEL', 'WORK_PATH', 'DATE_FORMAT', 'FILE_TYPE']
-modeldat_modules = ['MAXDT', 'GMTREFERENCE', 'DT_PREDICTION_INTERVAL']
+uniqueId_modules=['NAME', 'SIMULATED_DAYS', 'FILENAME_FROM_MODEL', 'WORKPATH', 'DATE_FORMAT', 'FILE_TYPE']
+modeldat_modules = ['MAXDT','GMTREFERENCE', 'DT_PREDICTION_INTERVAL']
 preprocessing_modules = ['#name of block']
 name_of_block_modules_pre=['RUN', 'DAT_DATE_CHANGE', 'WORKING_DIRECTORY', 'CONFIG_FILEPATH', 'EXE_PATH', 'FLAGS', 'OUTPUT_TO_FILE', 'OUTPUT_FILEPATH']
 postprocessing_modules = ['#name of block']
@@ -26,7 +28,7 @@ def create_mohid_block(filename):
         filename.write("    " + i + ":\n")
 
 def create_model_block(filename):
-    filename.write("MODEL:\n")
+    filename.write("model_name:\n")
     for i in model_modules:
         filename.write("  " + i + ":\n")
 
@@ -34,13 +36,17 @@ def create_discharges_block(filename):
     filename.write("  DISCHARGES:\n")
     for i in discharges_modules:
         filename.write("    " + i + ":\n")
+        for i in discharge_block_modules:
+            filename.write("      " + i + ":\n")
 
 def create_obc_block(filename):
     filename.write("  OBC:\n")
     for i in obc_modules:
-        if i == "files":
-            filename.write("    " + i + ": [<write list of files you want from the OBC workpath (e.g. ['Hydrodynamic', 'WaterProperties']). Different files require a new list>]\n")
         filename.write("    " + i + ":\n")
+        for i in obc_block_modules:
+            if i == "files":
+                filename.write("      " + i + ": [<write list of files you want from the OBC workpath (e.g. ['Hydrodynamic', 'WaterProperties']). Different files require a new list>]\n")
+            filename.write("      " + i + ":\n")
 
 def create_meteo_block(filename, n):
     filename.write("  METEO:\n")
@@ -79,7 +85,7 @@ def create_yaml():
     number_model_meteo_blocks = []
     
     for i in range(number_model_blocks):
-        t = int(input("NUMBER OF METEO BLOCKS FOR MODEL BLOCK NR." + str(i+1) + " : "))
+        t = int(input("NUMBER OF METEO BLOCKS FOR MODEL BLOCK NR." + str(i+1) + ": "))
         number_model_meteo_blocks.append(t)
 
     number_preprocessing_blocks = int(input("NUMBER OF PREPROCESSING BLOCKS: "))
@@ -87,11 +93,12 @@ def create_yaml():
 
     create_artconfig_block(f)
     create_mohid_block(f)
-    create_model_block(f)
-    create_discharges_block(f)
-    create_obc_block(f)
-    create_meteo_block(f, number_model_blocks)
-    create_model_dat_block(f)
+    for i in range(number_model_blocks):
+        create_model_block(f)
+        create_discharges_block(f)
+        create_obc_block(f)
+        create_meteo_block(f, number_model_blocks[i])
+        create_model_dat_block(f)
     create_preprocessing_block(f, number_preprocessing_blocks)
     create_postprocessing_block(f, number_postprocessing_blocks)
 
