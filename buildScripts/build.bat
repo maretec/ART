18cc6690d5a781d330d319660f696cda91f22794
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2020-10-19 18:27:50 +0100

    adds path question

--- buildScripts/build.bat
+++ buildScripts/build.bat
@@ -1,4 +1,5 @@
-python -m pip install pipenv
-python -m pipenv install --ignore-pipfile
-python -m pipenv run pyinstaller --onefile ../python/art.py
+set /p path=Enter Python3 exe path: 
+%path% -m pip install pipenv 
+%path% -m pipenv install --ignore-pipfile --python %path%
+%path% -m pipenv run pyinstaller --onefile ../python/art.py
 pause
