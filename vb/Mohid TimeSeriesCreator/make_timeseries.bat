631741066bfaf9a7d174fd72b75c9da1df63dae1
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2021-01-31 13:52:33 +0000

    Removes old files from previous versin

--- vb/Mohid TimeSeriesCreator/make_timeseries.bat
+++
@@ -1,1 +0,0 @@
-"Mohid TimeSeriesCreator.exe" -a0=Import_Dates -a1=D:\Aplica\OPTagus3D_V1\Work\Discharges\Tagus\discharges.tss -a2=2019-07-13 -a3=2019-07-14 > Discharges_2019-07-12_122054_3DTagusWQ.log
