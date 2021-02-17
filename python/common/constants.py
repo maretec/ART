6574488cffe6368baa3499ed749920d57a8427da
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2021-02-17 20:36:48 +0000

    Adds individual log for each run

--- python/common/constants.py
+++ python/common/constants.py
@@ -1,9 +1,13 @@
 import common.logger as logger
+from datetime import datetime
 
 DATE_FORMAT = '%Y %m %d %H %M %S'
 DATE_FOLDER_FORMAT = '%Y-%m-%d'
 
-logger = logger.ArtLogger("MOHID", "log.txt")
+now = datetime.now()
+now_str = now.strftime("%y-%m-%d_%H%M%S")
+
+logger = logger.ArtLogger("ART", "ART_" + now_str + ".log")
 WRITE_TRIGGER = "WRITE_TRIGGER"
 FOLDERS_TO_WATCH = "FOLDERS_TO_WATCH"
 TRIGGER_MAX_WAIT = "TRIGGER_MAX_WAIT"
