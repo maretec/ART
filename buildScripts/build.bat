dc9cec2b9258d49f8f3c5eff7a210968b9534d2e
Tiago Gonçalves <tiago.goncalves@ctw.bmwgroup.com>
2022-01-26 15:52:05 +0000

    Fix building modules

--- buildScripts/build.bat
+++ buildScripts/build.bat
@@ -14,5 +14,5 @@
 if NOT DEFINED py_path set /p py_path=Python path not defined. Please enter Python3 folder exe path: 
 %py_path%\python.exe -m pip install pipenv 
 %py_path%\python.exe -m pipenv install --ignore-pipfile --python %py_path%\python.exe
-%py_path%\python.exe -m pipenv run pyinstaller --onefile ../python/art.py
+%py_path%\python.exe -m pipenv run pyinstaller --onefile ../python/art.py --paths=../python/common/ --paths=../python/
 pause
