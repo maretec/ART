d8b22862460a31eff5e5cdf103db5fc78616040f
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2022-01-28 15:31:32 +0000

    Fixes mpi exe path reference

--- python/mohid/mohid_water.py
+++ python/mohid/mohid_water.py
@@ -203,12 +203,12 @@ class MohidWater:
             self.logger.info("Starting MOHID MPI")
             # cwd is the working directory where the command will execute. stdout is the output file of the command
             if 'EXE_PATH' in self.yaml['MOHID_WATER']['MPI']:
-                exe_path = Path(self.yaml['MOHID_WATER']['MPI']['EXE_PATH'])
+                mpi_exe_path = Path(self.yaml['MOHID_WATER']['MPI']['EXE_PATH'])
             else:
-                exe_path = Path(self.yaml['MOHID_WATER']['EXE_PATH'])
+                mpi_exe_path = "mpiexec"
                 self.logger.info(
                     "Executable information for MPI missing. Defaulting to main EXE: " + exe_path.__str__())
-            subprocess.run(["mpiexec", "-np", str(self.yaml['MOHID_WATER']['MPI']['TOTAL_PROCESSORS']),
+            subprocess.run([mpi_exe_path.__str__(), "-np", str(self.yaml['MOHID_WATER']['MPI']['TOTAL_PROCESSORS']),
                             exe_path, "&"], cwd=os.path.dirname(self.yaml['MOHID_WATER']['EXE_PATH']),
                            stdout=output_file)
             output_file.close()
